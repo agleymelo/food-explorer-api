7fceb08c6b746b11f9efe6844ba6a2f3d19267de
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-21 11:31:33 -0300

    make migrate create-users

---
+++ src/database/knex/migrations/20240321142449_create-users.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) =>
+  knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.text("email").unique().notNullable();
+    table.text("password").notNullable();
+
+    table
+      .enum("role", ["admin", "customer"], {
+        useNative: true,
+        enumName: "roles",
+      })
+      .notNullable()
+      .defaultTo("customer");
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable("users");
