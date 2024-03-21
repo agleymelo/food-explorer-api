79bd8fc97a6a3875f57b7e9d7c4587fceea32317
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-21 12:16:36 -0300

    create user controller

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,3 +1,24 @@
-class UsersController {}
+const { hash } = require("bcryptjs");
+
+const knex = require("../database/knex");
+const AppError = require("../utils/app-error");
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const userAlreadyExist = await knex("users").where({ email }).first();
+
+    if (userAlreadyExist) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({ name, email, password: hashedPassword });
+
+    return response.status(201).json();
+  }
+}
 
 module.exports = UsersController;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
+require("express-async-errors");
 const express = require("express");
+
+const AppError = require("./utils/app-error");
+const routes = require("./routes/index.routes");
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3333;
+app.use(routes);
 
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response
+      .status(error.statusCode)
+      .json({ status: "error", message: error.message });
+  }
+
+  console.log(error);
+
+  return response
+    .status(500)
+    .json({ status: "error", message: "Server Internal Error" });
+});
+
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`🔥 Server running at port ${PORT}`));
