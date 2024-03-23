aef11fd74f6a37b4e7a2d8d8752f546d83184eb2
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-22 21:25:09 -0300

    add pm2

--- src/server.js
+++ src/server.js
@@ -1,45 +1,30 @@
-require("express-async-errors");
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('express-async-errors')
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 
-const AppError = require("./utils/app-error");
-const routes = require("./routes/index.routes");
-const { UPLOADS_FOLDER } = require("./configs/upload");
+const AppError = require('./utils/app-error')
+const routes = require('./routes/index.routes')
+const { UPLOADS_FOLDER } = require('./configs/upload')
 
-const app = express();
+const app = express()
 
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://192.168.1.2:5173",
-      "http://127.0.0.1:5173",
-    ],
-    credentials: true,
-  }),
-);
-
-app.use("/files", express.static(UPLOADS_FOLDER));
-
-app.use(routes);
+app.use(express.json())
+app.use(cors())
+app.use('/files', express.static(UPLOADS_FOLDER))
+app.use(routes)
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
-    return response
-      .status(error.statusCode)
-      .json({ status: "error", message: error.message });
+    return response.status(error.statusCode).json({ status: 'error', message: error.message })
   }
 
-  console.log(error);
+  console.log(error)
 
-  return response
-    .status(500)
-    .json({ status: "error", message: "Server Internal Error" });
-});
+  return response.status(500).json({ status: 'error', message: 'Server Internal Error' })
+})
 
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => console.log(`🔥 Server running at port ${PORT}`));
+const PORT = process.env.PORT || 3333
+app.listen(PORT, () => console.log(`🔥 Server running at port ${PORT}`))
